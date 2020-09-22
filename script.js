6c256ae44bec812946ee3010ba518f240ce63a74
Josh Glugatch <joshglugatch@gmail.com>
2020-09-22 09:52:18 -0700

    add question array with objects

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 var startBtn = document.getElementById("startButton");
 startBtn.style.display = "block";
-var question = document.getElementById("question");
-question.style.display = "none";
+
+var answerBtn = document.querySelector(".answerButton");
+
+var questionEl = document.getElementById("question");
+questionEl.style.display = "none";
+
 var btn1 = document.getElementById("button1");
 btn1.style.display = "none";
 var btn2 = document.getElementById("button2");
@@ -11,16 +15,133 @@ btn3.style.display = "none";
 var btn4 = document.getElementById("button4");
 btn4.style.display = "none";
 
+var answerA = document.getElementById("A")
+var answerB = document.getElementById("B")
+var answerC = document.getElementById("C")
+var answerD = document.getElementById("D")
+
+
+//  QUESTIONS
+var questions = [
+    {
+         question: "Question 1",
+         answers: [
+        { text1: "other answer", correct: false },
+        { text2: "other answer", correct: false },
+        { text3: "answer 3 correct", correct: true },
+        { text4: "other answer", correct: false },
+         ]
+    },
+    {
+        question: "Question 2",
+        answers: [
+       { text1: "other answer", correct: false },
+       { text2: "answer 2 correct", correct: true },
+       { text3: "other answer", correct: false },
+       { text4: "other answer", correct: false },
+        ]
+   },
+    {
+        question: "Question 3",
+        answers: [
+       { text1: "other answer", correct: false },
+       { text2: "answer 2 correct", correct: true },
+       { text3: "other answer", correct: false },
+       { text4: "other answer", correct: false },
+        ]
+   },
+    {
+        question: "Question 4",
+        answers: [
+       { text1: "other answer", correct: false },
+       { text2: "answer 2 correct", correct: true },
+       { text3: "other answer", correct: false },
+       { text4: "other answer", correct: false },
+        ]
+   },
+    {
+        question: "Question 5",
+        answers: [
+       { text1: "other answer", correct: false },
+       { text2: "answer 2 correct", correct: true },
+       { text3: "other answer", correct: false },
+       { text4: "other answer", correct: false },
+        ]
+   },
+    {
+        question: "Question 6",
+        answers: [
+       { text1: "other answer", correct: false },
+       { text2: "answer 2 correct", correct: true },
+       { text3: "other answer", correct: false },
+       { text4: "other answer", correct: false },
+        ]
+   },
+    {
+        question: "Question 7",
+        answers: [
+       { text1: "other answer", correct: false },
+       { text2: "answer 2 correct", correct: true },
+       { text3: "other answer", correct: false },
+       { text4: "other answer", correct: false },
+        ]
+   },
+    {
+        question: "Question 8",
+        answers: [
+       { text1: "other answer", correct: false },
+       { text2: "answer 2 correct", correct: true },
+       { text3: "other answer", correct: false },
+       { text4: "other answer", correct: false },
+        ]
+   },
+    {
+        question: "Question 9",
+        answers: [
+       { text1: "other answer", correct: false },
+       { text2: "answer 2 correct", correct: true },
+       { text2: "other answer", correct: false },
+       { text4: "other answer", correct: false },
+        ]
+   },
+    {
+        question: "Question 10",
+        answers: [
+       { text1: "other answer", correct: false },
+       { text2: "answer 2 correct", correct: true },
+       { text3: "other answer", correct: false },
+       { text4: "other answer", correct: false },
+        ]
+   },
+];
+
 startBtn.addEventListener("click", begin)
+
+
 
 function begin(){
     startBtn.style.display = "none";
-    question.style.display = "block";
+    questionEl.style.display = "block";
     btn1.style.display = "block";
     btn2.style.display = "block";
     btn3.style.display = "block";
     btn4.style.display = "block";
+    
+    askQuestion();
+    
 } 
 
-var questions = []
+var i = 0
 
+function askQuestion(){
+
+    questionEl.textContent = questions[i].question;
+    answerA.textContent = questions[i].answers.text1;
+}
+// CALL NEXT QUESTION
+//answerBtn.addEventListener("click", askQuestion)
+
+
+function selectAnswer(){
+
+}
